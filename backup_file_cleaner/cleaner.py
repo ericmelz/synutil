7e977b09d4ff3c504a91dfb8ea6a01e2392ccb21
Eric Melz <emelz@gumgum.com>
2023-03-04 10:21:31 -0800

    work

--- backup_file_cleaner/cleaner.py
+++ backup_file_cleaner/cleaner.py
@@ -3,6 +3,7 @@ import re
 
 #BASE_DIR = '/volume1/homes/eric/_Documents/_Records/By Type/Jobs/LinkedIn/notes/voldemort_emails'
 BASE_DIR = '/volume1/homes/eric/_Documents/_Records/By Type/Jobs/LinkedIn/notes'
+COUNT_INTERVAL = 10
 
 
 def is_valid(name):
@@ -12,18 +13,29 @@ def is_valid(name):
     return not re.search(pattern, name)
 
 
-def crawl(directory):
-    listing = os.listdir(directory)
-    for item in listing:
-        path = BASE_DIR + '/' + item
-        if os.path.isdir(path):
-            crawl(path)
-        if not is_valid(item):
-            print(path)
+class Crawler:
+    def __init__(self, root):
+        self.root = root
+        self.counter = 0
+
+    def _crawl(self, directory):
+        listing = os.listdir(directory)
+        for item in listing:
+            if self.counter % COUNT_INTERVAL == 0:
+                print(self.counter)
+            self.counter += 1
+            path = self.root + '/' + item
+            if os.path.isdir(path):
+                self._crawl(path)
+            if not is_valid(item):
+                print(path)
+
+    def crawl(self):
+        self._crawl(self.root)
 
 
 def main():
-    crawl(BASE_DIR)
+    Crawler(BASE_DIR).crawl()
 
 
 if __name__ == '__main__':
