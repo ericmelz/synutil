c016c3f04eda46cc73bd9a649805538ad8116f61
Eric Melz <emelz@gumgum.com>
2023-03-04 10:25:31 -0800

    work

--- backup_file_cleaner/cleaner.py
+++ backup_file_cleaner/cleaner.py
@@ -2,7 +2,7 @@ import os
 import re
 
 #BASE_DIR = '/volume1/homes/eric/_Documents/_Records/By Type/Jobs/LinkedIn/notes/voldemort_emails'
-BASE_DIR = '/volume1/homes/eric/_Documents/_Records/By Type/Jobs/LinkedIn'
+BASE_DIR = '/volume1/homes/eric/_Documents/_Records/By Type/Jobs/LinkedIn/notes'
 COUNT_INTERVAL = 100
 
 
