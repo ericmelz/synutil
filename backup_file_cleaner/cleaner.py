d3aa54b469fae6e40b947239488f8658c949df0e
Eric Melz <emelz@gumgum.com>
2023-03-04 09:51:28 -0800

    work

--- backup_file_cleaner/cleaner.py
+++ backup_file_cleaner/cleaner.py
@@ -1,5 +1,13 @@
+import os
+
+BASE_DIR = '/volume1/homes/eric/_Documents/_Records/By Type/Jobs/LinkedIn/notes/voldemort_emails'
+
+
 def main():
     print("hey")
+    dir_list = os.listdir(BASE_DIR)
+    print(f'Files and directories:')
+    print(dir_list)
 
 
 if __name__ == '__main__':
