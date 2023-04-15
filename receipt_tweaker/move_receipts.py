3cbc84afac3ff87cbf403fc5113dbd4d30305ab6
Eric Melz <emelz@gumgum.com>
2023-04-15 12:19:57 -0700

    initial implementation of move_receipts.py

---
+++ receipt_tweaker/move_receipts.py
@@ -0,0 +1,53 @@
+import os
+import re
+import shutil
+from collections import Counter
+
+SRC_DIR = '/Users/emelz/Desktop/for filing/Receipts'
+DEST_DIR = '/tmp/receiptdemo'
+
+
+def transfer():
+    """
+    Transfer receipts from local disk to diskstation
+    :return:
+    """
+    pattern = r'(\d{4})_\d{2}_\d{2}_\d{2}.pdf'
+    count = 0
+    year_counter = Counter()
+    prefix_counter = Counter()
+    l1_prefix_counter = Counter()
+    prefix_len = len(SRC_DIR)
+    for root, dirs, files in os.walk(SRC_DIR, topdown=False):
+        for name in files:
+            m = re.match(pattern, name)
+            if m:
+                count += 1
+                year = m[1]
+                year_counter.update([year])
+                relative_path = root[prefix_len+1:]
+                l1_prefix = relative_path.split('/')[0]
+                prefix_counter.update([relative_path])
+                l1_prefix_counter.update([l1_prefix])
+                dest_path_prefix = os.path.join(DEST_DIR, year, 'Receipts', relative_path)
+                if not os.path.exists(dest_path_prefix):
+                    os.makedirs(dest_path_prefix)
+                dest_path = os.path.join(dest_path_prefix, name)
+                # print(f'{year}: {root} -- {relative_path}  ---   {name}')
+                src_path = os.path.join(root, name)
+                print(f'copying {src_path} to {dest_path}')
+                shutil.copyfile(src_path, dest_path)
+    print(f'Count is {count}')
+    print('\nYears:')
+    for year, count in year_counter.most_common():
+        print(f'{year} {count}')
+    print('\nPrefixes:')
+    for prefix, count in prefix_counter.most_common():
+        print(f'{prefix} {count}')
+    print('\nL1 Prefixes:')
+    for prefix, count in l1_prefix_counter.most_common():
+        print(f'{prefix} {count}')
+
+
+if __name__ == '__main__':
+    transfer()
