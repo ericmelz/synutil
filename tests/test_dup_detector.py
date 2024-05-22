8fd7f1fd5fc55884e2967080383244ad7d99082d
ericmelz <eric@emelz.com>
2024-05-22 15:04:51 -0700

    test_find_music_duplicates

--- tests/test_dup_detector.py
+++ tests/test_dup_detector.py
@@ -16,22 +16,28 @@ class MyTestCase(unittest.TestCase):
                 return True, file, lowercase_files[lowercase_file]
             lowercase_files[lowercase_file] = file
 
-    def test_case_insensitive_duplicates(self):
-        # Create a temporary directory and add test files
-        with tempfile.TemporaryDirectory() as tmpdir:
+    # def test_case_insensitive_duplicates(self):
+    #     # Create a temporary directory and add test files
+    #     with tempfile.TemporaryDirectory() as tmpdir:
+    #
+    #         print(f"Created temporary directory {tmpdir}")
+    #
+    #         path = Path(tmpdir)
+    #
+    #         # Add test files (change or add files as necessary for your test)
+    #         (path / "file.txt").write_text("content")
+    #         (path / "FILE.TXT").write_text("content")
+    #         (path / "another_file.txt").write_text("content")
+    #
+    #         # Perform the test
+    #         result, file1, file2 = self.find_case_insensitive_duplicates(tmpdir)
+    #         assert result, f"Files {file1} and {file2} are the same except for case"
 
-            print(f"Created temporary directory {tmpdir}")
+    def test_find_music_duplicates(self):
+        print("Testing find_music_duplicates")
 
-            path = Path(tmpdir)
-
-            # Add test files (change or add files as necessary for your test)
-            (path / "file.txt").write_text("content")
-            (path / "FILE.TXT").write_text("content")
-            (path / "another_file.txt").write_text("content")
-
-            # Perform the test
-            result, file1, file2 = self.find_case_insensitive_duplicates(tmpdir)
-            assert result, f"Files {file1} and {file2} are the same except for case"
+        result, file1, file2 = self.find_case_insensitive_duplicates("/music")
+        assert result, f"Files {file1} and {file2} are the same except for case"
 
 
 if __name__ == '__main__':
