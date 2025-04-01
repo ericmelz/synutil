3d1cf2d6aa6cc734bf23263d839b3ed0a8499b20
Eric Melz <eric@emelz.com>
2025-04-01 13:50:25 -0700

    initial work on compare_dirs

---
+++ source/compare_dirs/README.md
@@ -0,0 +1,2 @@
+# Compare dirs
+Compare 2 directories, highlighting differences in folder content sizes
--- source/external_backups/backup1.sh
+++ source/external_backups/backup1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./rsync.sh "/Volumes/homes/eric" "/Volumes/Backup8a/Backup/homes"
+#./rsync.sh "/Volumes/homes/eric" "/Volumes/Backup8a/Backup/homes"
 ./rsync.sh "/Volumes/homes/randi" "/Volumes/Backup8a/Backup/homes"
